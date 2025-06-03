7abb8647e9e8c39682b6bea2bee59e77827a527a
Katia-Alcocer <147543428+Katia-Alcocer@users.noreply.github.com>
2025-06-02 18:34:54 -0600

    Add files via upload
    Archivo de conexión para el php

--- conexion.php
+++ conexion.php
@@ -1,13 +1,16 @@
 <?php
-$dsn = "sqlsrv:Server=sqlserver_container,1433;Database=master";
+$dsn = "sqlsrv:Server=sqlserver_container,1433;Database=ProyectoHerreriaUG";
 $user = "sa";
 $password = "TuPassword123!";
 
 try {
     $pdo = new PDO($dsn, $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "¡Conectado con PDO sqlsrv!";
+    
 } catch (PDOException $e) {
     die("Error de conexión PDO: " . $e->getMessage());
 }
+
+
 ?>
+
